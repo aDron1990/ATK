885887fa6cd054f5cd43c7099fccfbb6d858dbe4
Шевелёв Андрей <andreysheveliov1990@gmail.com>
2021-10-23 23:38:29 +0300

    some little changes

--- ATK/src/main.cpp
+++ ATK/src/main.cpp
@@ -12,8 +12,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
 {
 	ATK::Window* window = new ATK::Window(hInstance, L"win", 800, 600);
 	ATK::EventHandler* e = ATK::EventHandler::getInstance();
-	
 
+	HMODULE hM = LoadLibrary(L"PluginManager");
+	void(*init)() = (void(*)())GetProcAddress(hM, "Init");
 
 	b = new ATK::Button(L"Êíîïêà", 0, 0, 100, 30);
 	b->setOnClick(gfeqwertf);
